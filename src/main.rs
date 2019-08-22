534b1940c351653a462593f10c08c5a50e8e1e00
Ákos Nádudvari <akos@nadudvari.org>
2019-08-22 21:30:31 +0200

    Basic paging

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,30 @@
 #![test_runner(rust_stuff::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use rust_stuff::{hlt_loop, init, println};
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use rust_stuff::memory;
+    use x86_64::{structures::paging::Page, VirtAddr};
+
     println!("Hello World{}", "!");
     crate::init();
+
+    let mut mapper = unsafe { memory::init(boot_info.physical_memory_offset) };
+    let mut frame_allocator =
+        unsafe { memory::BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    // map a previously unmapped page
+    let page = Page::containing_address(VirtAddr::new(0x1000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
