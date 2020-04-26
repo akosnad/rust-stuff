8bce127a0206f9298563a31b7fdb6d16f6428f09
akosnad <akos@nadudvari.org>
2020-04-26 18:06:48 +0200

    Implemented linked list allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -7,6 +7,7 @@ use x86_64::{
 };
 
 pub mod bump;
+pub mod linked_list;
 
 pub struct Locked<A> {
     inner: spin::Mutex<A>,
@@ -31,10 +32,11 @@ fn align_up(addr: usize, align: usize) -> usize {
     (addr + align - 1) & !(align - 1)
 }
 
-use bump::BumpAllocator;
+use linked_list::LinkedListAllocator;
 
 #[global_allocator]
-static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+static ALLOCATOR: Locked<LinkedListAllocator> = 
+    Locked::new(LinkedListAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
