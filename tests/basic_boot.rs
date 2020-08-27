34508a172a4a40c5e80b451083606d1c06550bbe
akosnad <akos@nadudvari.org>
2020-08-27 20:27:17 +0200

    Update deps & fix tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -9,9 +9,18 @@ extern crate rlibc;
 
 use core::panic::PanicInfo;
 use rust_stuff::{hlt_loop, println, serial_print, serial_println};
+use bootloader::BootInfo;
 
 #[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {
+    use rust_stuff::allocator;
+    use rust_stuff::memory::{self, BootInfoFrameAllocator};
+
+    let mut mapper = unsafe { memory::init(boot_info.physical_memory_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    
+    allocator::init_heap(&mut mapper, 1024 * 1024 * 4, &mut frame_allocator).expect("heap initialization failed");
+
     test_main();
     hlt_loop();
 }
